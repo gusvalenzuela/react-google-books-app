e1cb63386c3ca46d20e7f5b5d39401ff760bad20
Gus (Nala) <gusrosales7@gmail.com>
2020-06-10 23:10:20 -0700

    initializing

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# react-google-books-app
+# gOOgle bOOks Search (using ReactJS)
+
+> an app by gus Valenzuela
+
+# Description
+
+- Search Page - Search for books via the Google Books API. "View" (link to the book on Google Books) or "Save" a book, saving it to the site's database.
+
+- Saved Page - Renders all books saved to the database. "View" (link to the book on Google Books) or "Delete" a book, removing it from the site's database.
+
+## See it in Action
+
+https://[app-name].herokuapp.com/
---
+++ client/src/pages/Home.js
@@ -0,0 +1,125 @@
+import React, { Component } from "react";
+import Jumbotron from "../components/Jumbotron";
+import Card from "../components/Card";
+import Form from "../components/Form";
+import Book from "../components/Book";
+import Footer from "../components/Footer";
+import API from "../utils/API";
+import { Col, Row, Container } from "../components/Grid";
+import { List } from "../components/List";
+
+class Home extends Component {
+  state = {
+    books: [],
+    q: "",
+    message: "Search For A Book To Begin!",
+  };
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  getBooks = () => {
+    API.getBooks(this.state.q)
+      .then((res) =>
+        this.setState({
+          books: res.data,
+        })
+      )
+      .catch(() =>
+        this.setState({
+          books: [],
+          message: "No New Books Found, Try a Different Query",
+        })
+      );
+  };
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    this.getBooks();
+  };
+
+  handleBookSave = (id) => {
+    const book = this.state.books.find((book) => book.id === id);
+
+    API.saveBook({
+      googleId: book.id,
+      title: book.volumeInfo.title,
+      subtitle: book.volumeInfo.subtitle,
+      link: book.volumeInfo.infoLink,
+      authors: book.volumeInfo.authors,
+      description: book.volumeInfo.description,
+      image: book.volumeInfo.imageLinks.thumbnail,
+    }).then(() => this.getBooks());
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="jumbotron">
+              <h1 className="text-center">
+                <img
+                  style={{ display: "block", margin: "auto" }}
+                  alt="Google Logo Color"
+                  src="/images/googlelogo_color_272x92dp.png"
+                />
+                <strong>Books Search</strong>
+              </h1>
+              <h5 className="text-center">
+                Search for and Save Books of Interest.
+              </h5>
+            </div>
+          </div>
+          <div size="md-12">
+            <Card title="Book Search" icon="far fa-book">
+              <Form
+                handleInputChange={this.handleInputChange}
+                handleFormSubmit={this.handleFormSubmit}
+                q={this.state.q}
+              />
+            </Card>
+          </div>
+        </div>
+        <div>
+          <div className="col-md-12">
+            <Card title="Results">
+              {this.state.books.length ? (
+                <List>
+                  {this.state.books.map((book) => (
+                    <Book
+                      key={book.id}
+                      title={book.volumeInfo.title}
+                      subtitle={book.volumeInfo.subtitle}
+                      link={book.volumeInfo.infoLink}
+                      authors={book.volumeInfo.authors.join(", ")}
+                      description={book.volumeInfo.description}
+                      image={book.volumeInfo.imageLinks.thumbnail}
+                      Button={() => (
+                        <button
+                          onClick={() => this.handleBookSave(book.id)}
+                          className="btn btn-primary ml-2"
+                        >
+                          Save
+                        </button>
+                      )}
+                    />
+                  ))}
+                </List>
+              ) : (
+                <h2 className="text-center">{this.state.message}</h2>
+              )}
+            </Card>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Home;
